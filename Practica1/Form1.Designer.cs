eb67b156146e563f1981439b2506eaac99ab9bfe
Brian Morales <briansmor09@gmail.com>
2018-02-08 19:47:59 -0600

    Carga un archivo .lfp en un cuadro de texto

--- Practica1/Form1.Designer.cs
+++ Practica1/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.editor = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
@@ -36,14 +36,14 @@
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // editor
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(26, 37);
-            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(215, 312);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.editor.Location = new System.Drawing.Point(26, 37);
+            this.editor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.editor.Name = "editor";
+            this.editor.Size = new System.Drawing.Size(215, 312);
+            this.editor.TabIndex = 0;
+            this.editor.Text = "";
             // 
             // richTextBox2
             // 
@@ -63,6 +63,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Cargar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -104,7 +105,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.editor);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Identificador";
@@ -114,7 +115,7 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox editor;
         private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
--- Practica1/Form1.cs
+++ Practica1/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Practica1
 {
@@ -19,6 +20,22 @@ namespace Practica1
         private void button3_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Universidad San Carlos de Guatemala \n"+"Brian Morales \n"+"Lenguajes Formales de Programacion \n"+"Seccion A+ \n"+"Primer Semestre");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //-----Crea un objeto tip Openfile Dialog
+            OpenFileDialog abrirD = new OpenFileDialog();
+            //------Especifica el tipo de extension del archivo
+            abrirD.Filter = "|*.lfp";
+            //------Si se encuentra el archivo 
+            if (abrirD.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader lectura = new StreamReader(abrirD.FileName);
+                String inf = lectura.ReadToEnd();
+                editor.Text = inf;
+            }
+
         }
     }
 }
